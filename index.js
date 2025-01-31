7ab436cc52f006105d9e5265b10679b4bef06d70
unknown <katotodan@gmail.com>
2025-01-31 10:58:39 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get("/", (req, res)=>{
+    try {
+        res.status(200).json(
+            {
+                "email": "katotodan@gmail.com",
+                "current_datetime": new Date().toISOString(),
+                "github_url": "<https://github.com/yourusername/your-repo>"
+            }
+        )
+        
+    } catch (error) {
+        res.status(500).json(
+            {
+                "status": 500,
+                "request": "fails"
+            }
+        )
+    }
+    
+})
+
+app.listen("5000", ()=> console.log("Server running on port 5000"))
